6d718c1065dd46f7c4df05f6582cd66761366399
dk808hth <44477250+dk808hth@users.noreply.github.com>
2019-02-26 17:46:54 -0800

    Update queue.sh

--- queue.sh
+++ queue.sh
@@ -89,6 +89,8 @@ function _cache_command(){
     echo "$CONTENTS"
 }
 
+
+
 ZNLISTCMD_TMP=$(zelcash-cli listzelnodes 2>/dev/null)
 ZNLISTCMD=$(jq -r --arg tier ${ZNTIER} '[.[] |select(.tier==$tier) |{(.txhash):(.status+" "+(.version|tostring)+" "+.addr+" "+(.lastseen|tostring)+" "+(.activetime|tostring)+" "+(.lastpaid|tostring)+" "+.ipaddress)}]|add' <<< $ZNLISTCMD_TMP)
 
