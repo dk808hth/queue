f0ece51425a8581bc997d83a42373f0d3a95a68a
dk808hth <44477250+dk808hth@users.noreply.github.com>
2019-02-27 17:28:06 -0800

    Update queue.sh

--- queue.sh
+++ queue.sh
@@ -3,7 +3,6 @@
 ZNTIER=$1
 ZNADDR=$2
 
-GREEN='\033[1;32m'
 
 echo '                                                                      '
 echo -e '\033[1;34m                       #                       \033[0m   ' 
@@ -30,7 +29,7 @@ echo '            __        ___       ___                                    '
 echo '           /  \ |  | |__  |  | |__                                     '
 echo '           \__X \__/ |___ \__/ |___                                    '
 
-if [ -z $ZNTIER ]; then
+if [ -z "$ZNTIER" ]; then
     echo ""
     echo "usage   : $0 <tier> <zelnode address>"
     echo ""
@@ -38,11 +37,10 @@ if [ -z $ZNTIER ]; then
     echo "example : $0 -SUPER t1U4mLtUuiSwfVFS8rHCY1nANXD5fweP911"
     echo "example : $0 -BAMF t1MK2mtU8Wuoq22Z2FsMUMN41DsrGcFxcCo"
     echo ""
-    exit -1
 fi
 
 
-if [ -z $ZNADDR ]; then
+if [ -z "$ZNADDR" ]; then
     echo ""
     echo "usage   : $0 <tier> <zelnode address>"
     echo ""
@@ -50,35 +48,34 @@ if [ -z $ZNADDR ]; then
     echo "example : $0 -SUPER t1U4mLtUuiSwfVFS8rHCY1nANXD5fweP911"
     echo "example : $0 -BAMF t1MK2mtU8Wuoq22Z2FsMUMN41DsrGcFxcCo"
     echo ""
-    exit -1
 fi
 
 
-if [ $ZNTIER == -BASIC ] ; then
+if [ "$ZNTIER" == -BASIC ] ; then
     ZNTIER=BASIC
 fi
 
-if [ $ZNTIER == -SUPER ] ; then
+if [ "$ZNTIER" == -SUPER ] ; then
     ZNTIER=SUPER
 fi
 
-if [ $ZNTIER == -BAMF ] ; then
+if [ "$ZNTIER" == -BAMF ] ; then
     ZNTIER=BAMF
 fi
 
-ZNLISTCMD_TMP="`zelcash-cli listzelnodes 2>/dev/null`"
-ZNLISTCMD="`echo "$ZNLISTCMD_TMP" | jq -r '[.[] |select(.tier=="'${ZNTIER}'") |{(.txhash):(.status+" "+(.version|tostring)+" "+.addr+" "+(.lastseen|tostring)+" "+(.activetime|tostring)+" "+(.lastpaid|tostring)+" "+.ipaddress)}]|add'`"
+ZNLISTCMD_TMP="$(zelcash-cli listzelnodes 2>/dev/null)"
+ZNLISTCMD="$(echo "$ZNLISTCMD_TMP" | jq -r '[.[] |select(.tier=="'${ZNTIER}'") |{(.txhash):(.status+" "+(.version|tostring)+" "+.addr+" "+(.lastseen|tostring)+" "+(.activetime|tostring)+" "+(.lastpaid|tostring)+" "+.ipaddress)}]|add')"
 
 
 SORTED_ZN_LIST=$(echo "$ZNLISTCMD" | sed -e 's/[}|{]//' -e 's/"//g' -e 's/,//g' | grep -v ^$ | \
-awk ' \
+awk ' 
 {
     if ($7 == 0) {
         TIME = $6
         print $_ " " TIME
     }
     else {
-        xxx = ("'$NOW'" - $7)
+        xxx = ("$NOW" - $7)
         if ( xxx >= $6) {
             TIME = $6
         }
@@ -89,16 +86,16 @@ awk ' \
     }
 }' |  sort -k10 -n)
 
-ZN_VISIBLE=$(echo "$SORTED_ZN_LIST" | grep $ZNADDR | wc -l)
-ZN_LASTPAID=$(echo "$SORTED_ZN_LIST" | grep $ZNADDR | awk '{print $7}')
-ZN_LASTPAID=$(date -d @${ZN_LASTPAID})
+ZN_VISIBLE=$(echo "$SORTED_ZN_LIST" | grep -c "$ZNADDR")
+ZN_LASTPAID=$(echo "$SORTED_ZN_LIST" | grep "$ZNADDR" | awk '{print $7}')
+ZN_LASTPAID=$(date -d @"${ZN_LASTPAID}")
 ZN_QUEUE_LENGTH=$(echo "$SORTED_ZN_LIST" | wc -l)
-ZN_QUEUE_POSITION=$(echo "$SORTED_ZN_LIST" | grep -A9999999 $ZNADDR | wc -l)
-ZN_QUEUE_IN_SELECTION=$(( $ZN_QUEUE_POSITION <= $(( $ZN_QUEUE_LENGTH / 10 )) ))
+ZN_QUEUE_POSITION=$(echo "$SORTED_ZN_LIST" | grep -c -A9999999 "$ZNADDR")
+ZN_QUEUE_IN_SELECTION=$(( ZN_QUEUE_POSITION <= $(( ZN_QUEUE_LENGTH / 10 )) ))
 
 echo ""
-echo "Zelnode :" $ZNADDR
-if [ $ZN_VISIBLE -gt 0 ]; then
+echo "Zelnode :" "$ZNADDR"
+if [ "$ZN_VISIBLE" -gt 0 ]; then
     echo "Lastpaid: $ZN_LASTPAID"
     echo "Tier    : $ZNTIER"
         echo "         -> queue position $ZN_QUEUE_POSITION/$ZN_QUEUE_LENGTH"
